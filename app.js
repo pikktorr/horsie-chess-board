a1c3bb7c0b04cacca6e4bf7b3848cb30150f7849
pikktorr <viktor.rozgonyi@gmail.com>
2020-02-29 11:49:39 +0100

    only valid movements

--- app.js
+++ app.js
@@ -51,62 +51,34 @@ addFields();
 const squares = document.querySelectorAll(".field");
 const squareEvent = squares.forEach(square => {
   square.addEventListener("click", event => {
-    horsie.placeFigure(event);
-    clearHighlights();
-    showCurrentId(event);
-    moveTo();
+    if (moveCounter === 0) {
+      placeFigure(event);
+      showCurrentId(event);
+      moveTo();
+    } else if (event.target.className.includes("highlight")) {
+      placeFigure(event);
+      showCurrentId(event);
+      moveTo();
+    }
   });
 });
 
-//CHESS KNIGHT OBJECT W/ FUNCTIONS
-const horsie = {
-  name: "chess knight",
-  class: "horsie",
+let moveCounter = 0;
+console.log(moveCounter);
 
-  // numberOfMoves: {
-  //   eight: [
-  //     ["C3", "C4", "C5", "C6"],
-  //     ["D3", "D4", "D5", "D6"],
-  //     ["E3", "E4", "E5", "E6"],
-  //     ["F3", "F4", "F5", "F6"]
-  //   ],
-  //   six: [
-  //     ["B3", "B4", "B5", "B6"],
-  //     ["G3", "G4", "G5", "G6"],
-  //     ["C2", "D2", "E2", "F2"],
-  //     ["C7", "D7", "E7", "F7"]
-  //   ],
-  //   four: [
-  //     ["A3", "A4", "A5", "A6"],
-  //     ["H3", "H4", "H5", "H6"],
-  //     ["C1", "D1", "E1", "F1"],
-  //     ["C8", "D8", "E8", "F8"],
-  //     ["B2"],
-  //     ["B7"],
-  //     ["G2"],
-  //     ["G7"]
-  //   ],
-  //   three: [["A2"], ["A7"], ["B1"], ["B8"], ["G1"], ["G8"], ["H2"], ["H7"]],
-  //   two: [["A1"], ["A8"], ["H1"], ["H8"]]
-  // },
-
-  placeFigure: event => {
-    const figure = document.querySelector(".horsie");
-    const parent = event.target;
-    parent.appendChild(figure);
-  }
+const placeFigure = event => {
+  const figure = document.querySelector(".horsie");
+  const parent = event.target;
+  parent.appendChild(figure);
+  moveCounter++;
+  console.log(moveCounter);
+  clearHighlights();
 };
-
-// const moveTo = (currentField, num1, num2) => {
-//   const targetField = parseInt(currentField) + num1 * column + num2 * row;
-//   highlightTarget(targetField);
-// };
 
 let currentId;
 let idLetter;
 let idNumber;
 let targetId;
-// let targetFields = [];
 
 const showCurrentId = event => {
   currentId = [event.target.id[0], parseInt(event.target.id[1])];
@@ -120,12 +92,13 @@ const showTargetId = (num1, num2) => {
     lettersOfBoard[idLetter + num1],
     parseInt(numbersOfBoard[idNumber + num2])
   ];
+
   targetId[0] === undefined || numbersOfBoard.includes(targetId[1]) === false
-    ? console.log("i do nothing")
-    : highlightTarget(targetId[0] + targetId[1]);
-  console.log(targetId);
+    ? {} //if true, do nothing
+    : highlightTarget(targetId[0] + targetId[1]); // if false, do this
   return targetId;
 };
+
 const moveTo = () => {
   const oneOclock = showTargetId(1, -2);
   const twoOclock = showTargetId(2, -1);
@@ -146,40 +119,3 @@ const clearHighlights = () => {
   const targetDivs = document.querySelectorAll(".field");
   targetDivs.forEach(div => div.classList.remove("highlight"));
 };
-
-// switch (true) {
-//   case targetFields.eight
-//     .flat()
-//     .some(element => targetClass.includes(element)):
-//     oneOclock();
-//     twoOclock();
-//     fourOclock();
-//     fiveOclock();
-//     sevenOclock();
-//     eightOclock();
-//     tenOclock();
-//     elevenOclock();
-//     break;
-//   case targetFields.six[0]
-//     .flat()
-//     .some(element => targetClass.includes(element)):
-//     oneOclock();
-//     twoOclock();
-//     fourOclock();
-//     fiveOclock();
-//     sevenOclock();
-//     elevenOclock();
-//     break;
-//   case targetFields.six[1]
-//     .flat()
-//     .some(element => targetClass.includes(element)):
-//     oneOclock();
-//     fiveOclock();
-//     sevenOclock();
-//     eightOclock();
-//     tenOclock();
-//     elevenOclock();
-//     break;
-//   default:
-//     break;
-// }
